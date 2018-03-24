a0e0a4c0c9bb43be5ee655a6efbc7991c8fd1fd3
arun <arun.s.blore@gmail.com>
2018-03-24 20:06:35 +0530

    Mofified the properties for maven compilation

--- Apps/Spring/SpringApp2BBCh1ManualWiring/src/main/java/com/person/Car.java
+++ Apps/Spring/SpringApp2BBCh1ManualWiring/src/main/java/com/person/Car.java
@@ -1,26 +1,28 @@
 package com.person;
 
 public class Car {
+	
    private String model;
    private String color;
    
-/*   public Car(String model,String color){
+    /* public Car(String model,String color){
 	   this.model= model;
 	   this.color=color;
-   }*/
-public String getModel() {
-	return model;
-}
-public void setModel(String model) {
-	this.model = model;
-}
-public String getColor() {
-	return color;
-}
-public void setColor(String color) {
-	this.color = color;
-}
+     }*/
    
-   
-   
+    public String getModel() {
+	   return model;
+    }
+    
+    public void setModel(String model) {
+    	this.model = model;
+    }
+    
+    public String getColor() {
+    	return color;
+    }
+    
+    public void setColor(String color) {
+    	this.color = color;
+    }   
 }
--- Apps/Spring/SpringApp2BBCh1ManualWiring/src/main/java/com/person/Person.java
+++ Apps/Spring/SpringApp2BBCh1ManualWiring/src/main/java/com/person/Person.java
@@ -1,24 +1,27 @@
 package com.person;
 
 public class Person {
- private String name;
- private Car car1;
+   private String name;
+   private Car car1;
  
-/* public Person(Car car){
+   /* public Person(Car car){
 	 this.car = car;
- }*/
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public Car getCar() {
-	return car1;
-}
-public void setCar(Car car) {
-	this.car1 = car;
-}
- 
+   }*/
+   
+   public String getName() {
+	  return name;
+   }
+   
+   public void setName(String name) {
+	   this.name = name;
+   }
+   
+   public Car getCar() {
+	   return car1;
+   }
+   
+   public void setCar(Car car) {
+	  this.car1 = car;
+   }
  
 }
