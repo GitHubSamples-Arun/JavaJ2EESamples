dc37d1f684aad7401813b3ebafa5da0c10e5cd41
arun <arun.s.blore@gmail.com>
2018-10-21 02:46:36 +0530

    Included sample Spring Boot CRUD repository project

---
+++ Apps/SpringBoot/dept-crud-jersey/src/main/java/com/springboot/rest/deptcrudjersey/DeptCrudJerseyApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.rest.deptcrudjersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptCrudJerseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptCrudJerseyApplication.class, args);
+	}
+}
