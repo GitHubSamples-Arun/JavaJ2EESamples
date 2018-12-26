b2611574fcffed5556c8f02d2cc502bbf5d9199f
arun <arun.s.blore@gmail.com>
2018-12-27 01:46:58 +0530

    Included React sample projects

---
+++ Apps/React/dept-crud-react/src/test/java/com/springboot/rest/deptcrudjersey/DeptCrudJerseyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.rest.deptcrudjersey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeptCrudJerseyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
